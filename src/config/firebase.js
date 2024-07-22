e033f99f914a157a924a61e903c3e4b2932e37ee
Luis Mateo Michel Flores <101907819+Bussy888@users.noreply.github.com>
2024-07-22 01:34:58 -0400

    Actualizar firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,7 @@ import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/aut
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from 'firebase/firestore'; // Usa la API modular
 
-const firebaseConfig = {
-    apiKey: "AIzaSyB5oEQE6vJSBebnKIRcPIsWGz8vS0hxagk",
-    authDomain: "app-conciertos.firebaseapp.com",
-    projectId: "app-conciertos",
-    storageBucket: "app-conciertos.appspot.com",
-    messagingSenderId: "637400342824",
-    appId: "1:637400342824:web:8455ce61653d76cef5b01d"
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
