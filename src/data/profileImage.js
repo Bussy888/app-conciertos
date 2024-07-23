4bd122e0fea3b06d819c0e9703c8471488a644e6
Alexia Marin <alexiad.marinm@gmail.com>
2024-07-23 17:56:07 -0400

    profile

---
+++ src/data/profileImage.js
@@ -0,0 +1,13 @@
+const profileImages = [
+    'https://i.pinimg.com/originals/f1/fd/27/f1fd27abea1780b3c5da6151daa55682.jpg',
+    'https://i.pinimg.com/originals/14/04/76/140476816bc96eb99b04273abcdab6d5.jpg',
+    'https://i.pinimg.com/originals/2f/26/10/2f261037b5e955c012d69b5953891fa0.jpg',
+    'https://i.pinimg.com/originals/5e/94/a7/5e94a712cfa80a0358e598a6fd7ed9db.jpg',
+    'https://i.pinimg.com/originals/3a/e6/aa/3ae6aa27f8fd6eba4611232c156e089e.jpg',
+    'https://i.pinimg.com/originals/f1/9e/27/f19e279233f39a5d51c7f0addcc96d21.jpg',
+    'https://i.pinimg.com/originals/02/1b/9d/021b9d7d93f17eae24d6422a6fc89768.jpg',
+
+  ];
+  
+  export default profileImages;
+  
